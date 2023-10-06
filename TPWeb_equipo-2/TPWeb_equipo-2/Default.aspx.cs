8909ce0751e8c180d20e0f1ca5b731ef8f9ce4e8
Nahuel Alvez <nahue.alvez@gmail.com>
2023-10-06 14:40:00 -0300

    Ajustes

--- TPWeb_equipo-2/TPWeb_equipo-2/Default.aspx.cs
+++ TPWeb_equipo-2/TPWeb_equipo-2/Default.aspx.cs
@@ -11,10 +11,6 @@ namespace TPWeb_equipo_2
 {
     public partial class Default : System.Web.UI.Page
     {
-        private ImagenNegocio imagenNegocio;
-        private List<string> UrlImagenesArticulo;
-        private int IndiceImagen;
-        public Articulo articulo;
         public List<Articulo> ListaArticulo { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -22,10 +18,5 @@ namespace TPWeb_equipo_2
             ListaArticulo = negocio.Listar();
         }
 
-        private void CargarImagen()
-        {
-            UrlImagenesArticulo = imagenNegocio.GetImagenesNegocio(Articulo);
-            imagenNegocio.CargarImagenDesdeURL(UrlImagenesArticulo[IndiceImagen]);
-        }
     }
 }
